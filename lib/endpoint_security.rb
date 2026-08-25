8678d80c5596838a9c57b126df140995d23ed99c
ydah <t.yudai92@gmail.com>
2026-08-25 14:30:53 +0900

    Initial commit

---
+++ lib/endpoint_security.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "endpoint_security/version"
+
+module EndpointSecurity
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/endpoint_security_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EndpointSecurity do
+  it "has a version number" do
+    expect(EndpointSecurity::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
